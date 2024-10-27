ffd4bc0e71764609f6a79fe0bfc70b7eacc5acb1
XaerickSReborn <u202213358@upc.edu.pe>
2024-10-26 20:49:12 -0500

    chore: initial commit

---
+++ src/main/java/com/skillswap/platform/tutormatch/TutorMatchApplication.java
@@ -0,0 +1,13 @@
+package com.skillswap.platform.tutormatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorMatchApplication.class, args);
+	}
+
+}
