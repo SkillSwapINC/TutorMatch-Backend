6f66c219d5ecd26a47b969d403f51ca3d48f024f
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-11 19:01:40 -0500

    refactor(tutorings): update CreateTutoringSessionCommand with courseName and Course reference.

--- src/main/java/com/skillswap/platform/tutormatch/Tutorings/Domain/Model/Command/CreateTutoringSessionCommand.java
+++ src/main/java/com/skillswap/platform/tutormatch/Tutorings/Domain/Model/Command/CreateTutoringSessionCommand.java
@@ -1,7 +1,9 @@
 package com.skillswap.platform.tutormatch.Tutorings.Domain.Model.Command;
 
 
+import com.skillswap.platform.tutormatch.Tutorings.Domain.Model.Entities.Course;
 import com.skillswap.platform.tutormatch.Tutorings.Domain.Model.Entities.DailySchedule;
+
 import java.util.List;
 
 /**
@@ -10,7 +12,7 @@ import java.util.List;
  * including title, description,
  * price, available times, image, tutor ID, and course ID.
  *
- * @param title     The title of the tutoring session.
+ * @param courseName     The title of the tutoring session.
  * @param description The description of the tutoring session.
  * @param price     The price of the tutoring session.
  * @param times     A list of {@link DailySchedule} objects representing available
@@ -18,14 +20,15 @@ import java.util.List;
  * Each {@link DailySchedule} specifies the day and available hours.
  * @param image     The URL or path to an image associated with the tutoring session.
  * @param tutorId   The ID of the tutor who offers this tutoring session.
- * @param courseId  The ID of the course associated with this tutoring session.
+ * @param course  The ID of the course associated with this tutoring session.
  */
 public record CreateTutoringSessionCommand(
-        String title,
+        String courseName,
         String description,
         Double price,
         List<DailySchedule> times,
         String image,
         Long tutorId,
-        Long courseId
+        Long courseId,
+        Course course
 ) {}
