aef66421e01d48c96e7eeba61d68ab35c2c6957b
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-12 03:03:24 -0500

    refactor(users): simplify Role entity.

--- src/main/java/com/skillswap/platform/tutormatch/Users/Domain/Model/ValueObjects/Role.java
+++ src/main/java/com/skillswap/platform/tutormatch/Users/Domain/Model/ValueObjects/Role.java
@@ -1,23 +1,19 @@
 package com.skillswap.platform.tutormatch.Users.Domain.Model.ValueObjects;
 
-import java.util.concurrent.atomic.AtomicLong;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
 
-public record Role(RoleType roleType, Long tutorId) {
-    private static final AtomicLong tutorIdCounter = new AtomicLong(0);
+@Setter
+@Getter
+@Embeddable
+public class Role {
+    private RoleType roleType;
+
+    public Role() {}
 
     public Role(RoleType roleType) {
-        this(roleType, roleType == RoleType.teacher ? generateTutorId() : null);
+        this.roleType = roleType;
     }
 
-    private static Long generateTutorId() {
-        return tutorIdCounter.incrementAndGet();
-    }
-
-    public RoleType roleType() {
-        return roleType;
-    }
-
-    public Long tutorId() {
-        return tutorId;
-    }
 }
