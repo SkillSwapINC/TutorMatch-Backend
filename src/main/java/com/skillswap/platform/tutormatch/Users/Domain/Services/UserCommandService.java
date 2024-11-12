254ca4a896fba688626278b327821d3acf097740
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-12 01:51:26 -0500

    feat(users): add update user command handle.

--- src/main/java/com/skillswap/platform/tutormatch/Users/Domain/Services/UserCommandService.java
+++ src/main/java/com/skillswap/platform/tutormatch/Users/Domain/Services/UserCommandService.java
@@ -2,6 +2,8 @@ package com.skillswap.platform.tutormatch.Users.Domain.Services;
 
 import com.skillswap.platform.tutormatch.Users.Domain.Model.Aggregates.User;
 import com.skillswap.platform.tutormatch.Users.Domain.Model.Command.CreateUserCommand;
+import com.skillswap.platform.tutormatch.Users.Domain.Model.Command.UpdateUserCommand;
+
 import java.util.Optional;
 
 /**
@@ -16,4 +18,14 @@ public interface UserCommandService {
      * @return an {@link Optional} containing the created {@link User} if successful, or empty if not
      */
     Optional<User> handle(CreateUserCommand command);
+
+    /**
+     * Updates an existing user based on the provided command data.
+     *
+     * <p>This method processes the update request and returns an {@link Optional} containing the updated
+     * {@link User} if the operation is successful.
+     * If the update fails or the user is not found, an empty {@link Optional} is returned.
+     *
+     */
+    Optional<User> handle(UpdateUserCommand command);
 }
