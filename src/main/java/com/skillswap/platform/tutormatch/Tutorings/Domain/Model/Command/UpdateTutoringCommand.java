16342fee981ee53f3211fa4b8c664b187e868431
XaerickSReborn <u202213358@upc.edu.pe>
2024-11-14 04:17:26 -0500

    feat(tutorings): add UpdateTutoringCommand record.

---
+++ src/main/java/com/skillswap/platform/tutormatch/Tutorings/Domain/Model/Command/UpdateTutoringCommand.java
@@ -0,0 +1,7 @@
+package com.skillswap.platform.tutormatch.Tutorings.Domain.Model.Command;
+
+/**
+ * Represents a command to update an existing tutoring session.
+ */
+public record UpdateTutoringCommand() {
+}
